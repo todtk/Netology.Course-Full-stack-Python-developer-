468b24694d507cd34d751f7b0bcba0057cda2b2e
todtk <batkatod#gmail.com>
2023-03-23 22:35:18 +0500

    move from hw_4 to hw_4_iterators.generators.yield

--- homeworks/advance/hw_4_iterators.generators.yield/task_1/main.py
+++ homeworks/advance/hw_4_iterators.generators.yield/task_1/main.py
@@ -7,26 +7,19 @@ class FlatIterator:
         self.list_of_lists = list_of_lists
 
     def __iter__(self):
-        self.outer_list_cursor = 0
-        self.inner_list_cursor = 0
         return self
 
-    def __next__(self) -> str | bool | None:
+    def __next__(self):
+        if self.list_of_lists:
 
-        if self.list_of_lists[self.outer_list_cursor:]:
+            for inner_list in self.list_of_lists:
+                item = inner_list.pop(0)
 
-            for inner_list in self.list_of_lists[self.outer_list_cursor:]:
-
-                for item in inner_list[self.inner_list_cursor:]:
-
-                    if len(inner_list) != self.inner_list_cursor + 1:
-                        self.inner_list_cursor += 1
-                    else:
-                        self.outer_list_cursor += 1
-                        self.inner_list_cursor = 0
-                        
-                    return item
-
+                if not inner_list:
+                    self.list_of_lists.pop(0)
+                
+                return item
+                    
         else:
             raise StopIteration   
 

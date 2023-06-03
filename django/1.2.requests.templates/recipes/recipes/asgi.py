d990d0a32a21b586fca01a755d5a7dd2aedb8c02
todtk <batkatod@gmail.com>
2023-06-03 23:47:12 +0500

    added recipes

--- django/1.2.requests.templates/recipes/recipes/asgi.py
+++ django/1.2.requests.templates/recipes/recipes/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for requests_templates project.
+ASGI config for recipes project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'requests_templates.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipes.settings')
 
 application = get_asgi_application()

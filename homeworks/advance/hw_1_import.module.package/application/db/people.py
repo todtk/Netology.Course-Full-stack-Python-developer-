9871affe758ba6745072161b868fb46ce6f94398
todtk <batkatod@gmail.com>
2023-06-03 18:38:14 +0500

    removed

--- homeworks/advance/hw_1_import.module.package/application/db/people.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-def get_employees() -> str:
-    return "CALLED get_employees"
--- homeworks/advance/hw_1_import.module.package/application/salary.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-def calculate_salary() -> str:
-    return "CALLED calculate_salary"

faa356ee189e9e5b3a4b119fd3e5c463aecd10f4
todtk <batkatod@gmail.com>
2023-06-08 22:04:41 +0500

    registered url recipes/

--- django/1.2.requests.templates/recipes/recipes/urls.py
+++ django/1.2.requests.templates/recipes/recipes/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 
 from django.urls import path
+from calculator import views
+
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path("recipes/<name>", views.get_recipes),
 ]

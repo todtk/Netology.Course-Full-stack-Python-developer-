9627162826099ee1a6708ed4e9044180e269e0c1
todtk <batkatod@gmail.com>
2023-06-25 22:12:52 +0500

    added logic to show_catalog(), show_product()

--- django/2.1.databases/work_with_database/phones/views.py
+++ django/2.1.databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,29 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort')
+
+    if sort == "min_price":
+        sort_method = 'price'
+    elif sort == "max_price":
+        sort_method = '-price'
+    else:
+        sort_method = 'name'
+
+    context = {
+        'phones': Phone.objects.order_by(sort_method),
+    }
+
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+
+    phone = Phone.objects.filter(slug=slug)[0]
+
+    context = {
+        'phone': phone,
+    }
+
     return render(request, template, context)

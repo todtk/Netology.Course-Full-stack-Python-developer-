17c4e6a26ff2bf4ec82442225e445053f60610cc
todtk <batkatod@gmail.com>
2023-06-25 22:11:55 +0500

    added Phone model

--- django/2.1.databases/work_with_database/phones/models.py
+++ django/2.1.databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField()
+    price = models.IntegerField()
+    image = models.CharField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
